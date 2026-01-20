7f5aee488800dc0ad29d0298113342a1dbdcaac7
Sabid <sajjadsabid0@gmail.com>
2026-01-20 16:41:39 +0600

    improve axios secure hook and error handling

--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
+import { AuthContext } from '../context/AuthContext/AuthContext';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create(({
     baseURL: 'http://localhost:3000'
 }))
 
 const useAxiosSecure = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const navigate = useNavigate();
+    console.log(user)
+    useEffect(() => {
+        const reqInterceptor = axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user.accessToken}`;
+            return config;
+        })
+        const resInterceptor = axiosSecure.interceptors.response.use((response) => {
+            return response;
+        },
+            (error) => {
+                const statusCode = error.status;
+                if (statusCode === 401 || statusCode === 403) {
+                    logOut()
+                        .then(() => {
+                            navigate('/login')
+                        })
+                }
+                return Promise.reject(error)
+            })
+        return () => {
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        }
+
+    }, [user])
     return axiosSecure;
 };
 
